b4f59d7354406c7adc3a127b03a876236143ab46
Ritik Porwal <109209220+ritikporwal710@users.noreply.github.com>
2025-02-22 17:44:10 +0530

    Time: 4 ms (2.25%), Space: 13.2 MB (9.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
-        int i=0,j=0;
-        int p = m-n;
-        cout<<"true"<<endl;
-        while(i<m && j<n){
-            // cout<<i<<" "<<j<<endl;
-            if(v2[j]<v1[i]){
-                // cout<<v2[j]<<" "<<v1[i]<<endl;
-                swap(v2[j],v1[i]);
-                sort(v2.begin(),v2.end());
-                i++;
-                // j++;
-            }
-            else{
-                i++;
-            }
-        }
-        while(j!=n){
-            v1[i++] = v2[j++];
+        int i =0,j=0;
+        int p = 0;
+        // if(n==0) return ;
+        // if(m==0) return ;
+        vector<int> ans(n,0);
+
+        for(int i=0;i<m;++i){
+            // v1[n+i] = v1[i];
+            // swap(v1[n+i], v1[i]); 
+            // v1.pop_back();
+            ans.push_back(v1[i]);
         }
 
+        v1 = ans;
+
+        //    for(auto x: v1){
+        //     cout<<x<<" ";
+        // }
+
+        
+
+        while(i!=m && j!=n){
+            if(v1[n+i]<=v2[j]){
+                swap(v1[p], v1[n+i]);
+                i++;
+            }
+            else{
+                swap(v1[p], v2[j]);
+                j++;
+            }
+            p++;
+        }
+
+        while(i!=m){
+            swap(v1[p], v1[n+i]);
+            i++;p++;
+        }
+        while(j!=n){
+            cout<<j<<endl;
+            swap(v1[p], v2[j]);
+            j++;p++;
+        }
+       
+        
     }
 };
+      
