d2c8043daecfe8182b3abee89cb86dc9f15bbb56
Ritik Porwal <109209220+ritikporwal710@users.noreply.github.com>
2025-03-10 00:28:06 +0530

    Time: 0 ms (100%), Space: 71.6 MB (80.84%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& v) {
+        int n = v.size(),maxi = v[0],sum=0;
+        for(int i =0;i<n;++i){
+            sum += v[i];
+            maxi = max(maxi, sum);
+            if(sum<0) sum = 0;
+        }
+        return maxi;
+    }
+};
