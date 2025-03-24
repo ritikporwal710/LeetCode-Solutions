4bc6937ed2c98266f625d2e0b8776b9b2ea4cd23
Ritik Porwal <109209220+ritikporwal710@users.noreply.github.com>
2025-03-25 01:04:37 +0530

    Time: 0 ms (100%), Space: 17.8 MB (71.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,20 +4,14 @@ public:
         int n = v.size();
         priority_queue<pair<int,int>> pq;
         vector<int> ans;
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for(int i =0;i<n;++i)
         mp[v[i]]++;
 
-        for(auto x: mp){
-            pq.push({x.second,x.first});
-        }
-
+        for(auto x: mp) pq.push({x.second,x.first});
     
-        while(pq.empty() == false && k>0){
-            int a = pq.top().first,b=pq.top().second;
-            ans.push_back(b);
-            pq.pop();
-            k--;
+        while(pq.empty() == false && k-- >0){
+            ans.push_back(pq.top().second); pq.pop();
         }
     
 
